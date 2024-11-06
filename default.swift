fa441c4303bd323477426b22711512505e077bb6
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2024-11-06 10:16:37 -0800

    Update default.swift

--- default.swift
+++ default.swift
@@ -7,7 +7,7 @@ extension AppDelegate {
                 name: "Feather Beta Repository",
                 id: "kh.crysalis.feather-repo.beta",
                 iconURL: URL(string: "https://raw.githubusercontent.com/khcrysalis/Feather/refs/heads/main/Images/feather_beta.png"),
-                url: "https://raw.githubusercontent.com/ssalggnikool/.github/refs/heads/main/featherbeta-2nui218.json")
+                url: "https://raw.githubusercontent.com/ssalggnikool/.github/refs/heads/main/b.json")
             {_ in
                 Debug.shared.log(message: "Added beta default repos!")
                 Preferences.bDefaultRepos = true
