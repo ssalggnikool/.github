f6efc475763445ca2b8bad95a9f92c43af676976
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2024-11-06 11:22:48 -0800

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f iOS/Delegates/AppDelegate.swift ]; then
-    cat Bbuild/default.swift >> iOS/Delegates/AppDelegate.swift
+    cat $GITHUB_WORKSPACE/Bbuild/default.swift >> $GITHUB_WORKSPACE/Feather/iOS/Delegates/AppDelegate.swift
     sed -i '' '/func application(/a\
         addBDefaultRepos();
         ' iOS/Delegates/AppDelegate.swift
