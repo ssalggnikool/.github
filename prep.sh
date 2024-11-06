a29fd679fb29240579b112d7ec70fc9e6cb550d3
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2024-11-06 10:21:51 -0800

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-cat default.swift >> ./iOS/Delegates/AppDelegate.swift
+if [ -f iOS/Delegates/AppDelegate.swift ]; then
+    cat default.swift >> iOS/Delegates/AppDelegate.swift
+    sed -i '' '/func application(/a\
+        addBDefaultRepos();
+        ' iOS/Delegates/AppDelegate.swift
+
+else
+    echo "AppDelegate.swift does not exist."
+fi
