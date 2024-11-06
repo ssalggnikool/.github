4bd215fdd728df38490b96df827663b20dac0fc4
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2024-11-06 11:36:23 -0800

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -4,7 +4,7 @@ if [ -f iOS/Delegates/AppDelegate.swift ]; then
     cat $GITHUB_WORKSPACE/Bbuild/default.swift >> $GITHUB_WORKSPACE/Feather/iOS/Delegates/AppDelegate.swift
     sed -i '' '/func application(/a\
         addBDefaultRepos();
-        ' $GITHUB_WORKSPACE/iOS/Delegates/AppDelegate.swift
+        ' $GITHUB_WORKSPACE/Feather/iOS/Delegates/AppDelegate.swift
 
 else
     echo "AppDelegate.swift does not exist."
